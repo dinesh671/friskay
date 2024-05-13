852727866b9ded0044bc8b83cbcaf7ed1a71ca33
DvsDineshKumar <dasarivenkta.kumar@perficient.com>
2024-05-13 14:46:41 +0530

    some enhancements

--- script.js
+++ script.js
@@ -9,3 +9,20 @@ menuBtn.addEventListener('click', () => {
 closeBtn.addEventListener('click', () => {
   menu.classList.remove('open');
 });
+
+    document.getElementById('contactForm').addEventListener('submit', function(event) {
+        event.preventDefault(); // Prevent the default form submission
+        
+        // Fetching form data
+        var formData = {
+            name: document.getElementById('name').value,
+            email: document.getElementById('email').value,
+            phone: document.getElementById('phone').value,
+            address: document.getElementById('address').value,
+            packs: document.getElementById('packs').value,
+            reason: document.getElementById('reason').value
+        };
+
+        // Logging form data to the console
+        console.log(formData);
+    });
